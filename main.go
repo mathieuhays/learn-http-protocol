c4719c13a6db1a1e21466c31789ba205a719cc99
Mathieu Hays <5703113+mathieuhays@users.noreply.github.com>
2025-03-17 22:59:08 +0100

    ch1.l3

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+const inputFilePath = "messages.txt"
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	f, err := os.Open(inputFilePath)
+	if err != nil {
+		log.Fatalf("could not open %s: %v", inputFilePath, err)
+	}
+
+	fmt.Printf("Reading data from %s\n", inputFilePath)
+	fmt.Println("========================================")
+
+	for {
+		bytes := make([]byte, 8)
+		n, err := f.Read(bytes)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+
+			fmt.Printf("error: %s\n", err.Error())
+		}
+
+		str := string(bytes[:n])
+		fmt.Printf("read: %s\n", str)
+	}
 }
