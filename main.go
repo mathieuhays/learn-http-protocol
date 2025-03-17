65a6b8e4ed4f798b8ca5dc4813366d316f249b1e
Mathieu Hays <5703113+mathieuhays@users.noreply.github.com>
2025-03-17 23:19:02 +0100

    ch1.l5

--- main.go
+++ main.go
@@ -20,34 +20,50 @@ func main() {
 	fmt.Printf("Reading data from %s\n", inputFilePath)
 	fmt.Println("========================================")
 
-	currentLine := ""
-
-	for {
-		bytes := make([]byte, 8)
-		n, err := f.Read(bytes)
-		if err != nil {
-			if currentLine != "" {
-				fmt.Printf("read: %s\n", currentLine)
-				currentLine = ""
-			}
-
-			if errors.Is(err, io.EOF) {
-				break
-			}
-
-			fmt.Printf("error: %s\n", err.Error())
-		}
-
-		str := string(bytes[:n])
-
-		parts := strings.Split(str, "\n")
-		for i, part := range parts {
-			if i > 0 {
-				fmt.Printf("read: %s\n", currentLine)
-				currentLine = ""
-			}
-
-			currentLine += part
-		}
+	for line := range getLinesChannel(f) {
+		fmt.Printf("read: %s\n", line)
 	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+
+	go func() {
+		defer f.Close()
+		defer close(ch)
+		currentLine := ""
+
+		for {
+			bytes := make([]byte, 8)
+			n, err := f.Read(bytes)
+			if err != nil {
+				if currentLine != "" {
+					//fmt.Printf("read: %s\n", currentLine)
+					ch <- currentLine
+					currentLine = ""
+				}
+
+				if errors.Is(err, io.EOF) {
+					break
+				}
+
+				fmt.Printf("error: %s\n", err.Error())
+			}
+
+			str := string(bytes[:n])
+
+			parts := strings.Split(str, "\n")
+			for i, part := range parts {
+				if i > 0 {
+					//fmt.Printf("read: %s\n", currentLine)
+					ch <- currentLine
+					currentLine = ""
+				}
+
+				currentLine += part
+			}
+		}
+	}()
+
+	return ch
 }
